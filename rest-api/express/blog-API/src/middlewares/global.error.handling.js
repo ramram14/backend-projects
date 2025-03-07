e161c53be1adc6816b0b045c5bb0816def7e3835
Ikram <ikrammuh1402@gmail.com>
2025-03-07 15:22:52 +0700

    update error handling

--- rest-api/express/blog-API/src/middlewares/global.error.handling.js
+++ rest-api/express/blog-API/src/middlewares/global.error.handling.js
@@ -1,14 +1,33 @@
+import mongoose from "mongoose";
 // 🔥 Global Error Handling Middleware
 const globalErrorhandling = ((err, req, res, next) => {
-    console.log(err);
+    console.log(err.message);
+
+    // Handle CastError (Invalid ObjectId or Type Errors)
+    if (err instanceof mongoose.Error.CastError) {
+        const message = `Invalid ${err.path}: ${err.value}`;
+        err.message = message;
+        err.statusCode = 400;
+    }
+
+    // Handle ValidationError (Schema validation errors)
+    if (err instanceof mongoose.Error.ValidationError) {
+        err.statusCode = 400;
+        err.message = "Validation Error";
+        const errors = Object.values(err.errors).map(el => el.message); // Mengambil semua pesan error dari Mongoose
+        err.errors = errors;
+    }
+
+
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
-    const errors = err.errors;
+    const errors = err.errors || null;
+
 
     res.status(statusCode).json({
         success: false,
-        message: message,
-        errors: errors,
+        message,
+        errors
     });
 });
 
